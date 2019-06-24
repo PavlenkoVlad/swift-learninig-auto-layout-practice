6b82bbb1369c97742413cff7e298e3a0e7e790fe
Vlad Pavlenko <pavlenko0902@gmail.com>
2019-06-24 13:28:01 +0300

    init commit

---
+++ AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by Владислав Павленко on 4/26/19.
+//  Copyright © 2019 Владислав Павленко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
